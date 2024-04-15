e7c1b3c763c3eada1e89616056d48f740165c8a1
Mane <mane.ghazaryan600@gmail.com>
2024-04-15 21:36:04 +0400

    test commit

--- MindSwap.Domain/Category.cs
+++ MindSwap.Domain/Category.cs
@@ -12,5 +12,6 @@ namespace MindSwap.Domain
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public ICollection<Post>? Posts { get; set; }
+        
     }
 }
